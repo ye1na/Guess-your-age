236b6ac18e4f404706ab4b684f11aecd21270d1a
Valentine Olivier <116736392+ye1na@users.noreply.github.com>
2025-09-20 14:16:04 +0200

    Add files via upload

---
+++ Guess_your_age.py
@@ -0,0 +1,7 @@
+print("Hello, I'm Bob")
+Name = input("Your name ?")
+print("Hello", Name)
+Current_Year = 2025
+Birth_Year = int(input("What's your birth year ?"))
+Age = Current_Year - Birth_Year
+print(Name, ",", "Your age is", Age)
